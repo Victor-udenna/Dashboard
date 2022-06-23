2d9b6437e97855612ae40da2f88f9146f32221e4
victorudenna@gmail.com <victorudenna@gmail.com>
2022-06-23 16:16:01 +0100

    Added see all toggle function

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("Unable to load chart feed, try reconnectiong to a stronger internet connection");
+// alert("Unable to load chart feed, try reconnectiong to a stronger internet connection");
+
+function showMore() {
+    document.querySelector('#hidden-element').classList.toggle('open')
+}
+
+let btn = document.querySelector('#seeall_btn');
+
+btn.addEventListener('click', showMore)
